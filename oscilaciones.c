40bb4263bc9e319f0d8817bbfb431d70a8877f1e
Andrea Velasquez <a.velasquez12@uniandes.edu.co>
2017-02-25 23:34:03 -0500

    crea funciones x0 y acc

--- oscilaciones.c
+++ oscilaciones.c
@@ -3,8 +3,13 @@
 #include <math.h>
 #include <mpi.h>
 
+#define PI 3.14159265
+
 int N = 1024;
 float B = 0.3, dt = 0.001;
+
+double *x_0(int n);
+double *acceleration(int n);
 
 int main(void){
  
@@ -22,5 +27,21 @@ int main(void){
 
   MPI_Finalize();
   return 0;
+
+}
+
+
+
+double *x_0(int n){
+
+  return sin(2*PI*n/(N-1));
+
+}
+
+
+
+double *acceleration(int n, double *x){
+
+  return (x[n+1]-2*x[n]+x[n-1]) + B*(pow((x[n+1]-x[n]), 3)-pow((x[n]-x[n-1]), 3));
 
 }
