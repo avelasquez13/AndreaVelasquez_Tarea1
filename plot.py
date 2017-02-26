a0eeb176c6e1f31ca533a11904999eb0a2a49621
Andrea Velasquez <a.velasquez12@uniandes.edu.co>
2017-02-25 22:31:13 -0500

    crea plot.py

---
+++ plot.py
@@ -0,0 +1,10 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+datos = np.loadtxt('valores.dat')
+
+plt.imshow(datos, extent = [0, 5, 0, 5])
+plt.ylim([0, 5])
+
+plt.title('Posicion de cada atomo en funcion del tiempo')
+plt.savefig('grafica.pdf')
