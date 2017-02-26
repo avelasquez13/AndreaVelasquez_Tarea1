d578d6c72a14bd934d771ecde0a6ba03fa013d4f
Andrea Velasquez <a.velasquez12@uniandes.edu.co>
2017-02-25 22:39:38 -0500

    add colorbar

--- plot.py
+++ plot.py
@@ -4,6 +4,7 @@ import matplotlib.pyplot as plt
 datos = np.loadtxt('valores.dat')
 
 plt.imshow(datos, extent = [0, 5, 0, 5])
+plt.colorbar()
 plt.ylim([0, 5])
 
 plt.title('Posicion de cada atomo en funcion del tiempo')
