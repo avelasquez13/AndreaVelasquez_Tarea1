83cc26fb9ea7ac407feae78dcfbd675f9802edac
Andrea Velasquez <a.velasquez12@uniandes.edu.co>
2017-03-05 23:13:05 -0500

    vuelve scatter

--- tiempos.py
+++ tiempos.py
@@ -6,9 +6,10 @@ import matplotlib.pyplot as plt
 tiempos = np.loadtxt('tiempos.dat')
 procesador = [1, 2, 4]
 
-plt.plot(procesador, tiempos)
+plt.scatter(procesador, tiempos)
 plt.title('Tiempo que se demora corriendo el codigo segun numero de procesadores')
 plt.xlabel('Numero de procesadores')
-plt.ylabel('Tiempo')
+plt.ylabel('Tiempo (s)')
+plt.xticks([0, 1, 2, 3, 4, 5])
 plt.savefig('tiempos.pdf')
 plt.close()
