6c0cfa653acdc54e0587f8fc230a3ee751e4dfaa
Andrea Velasquez <a.velasquez12@uniandes.edu.co>
2017-03-05 22:05:33 -0500

    update plot.py

--- plot.py
+++ plot.py
@@ -1,13 +1,17 @@
 import numpy as np
+import matplotlib
+matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 
-datos = np.loadtxt('valores.dat')
 
+datos = np.loadtxt('valores.dat')
 energias=np.loadtxt('energias.dat')
 
 E1=energias[0]
 E2=energias[1]
 E3=energias[2]
+
+t = np.linspace(0, 47050, 1000)
 
 plt.imshow(datos)
 plt.colorbar()
@@ -16,7 +20,11 @@ plt.title('Posicion de cada atomo en funcion del tiempo')
 plt.savefig('grafica.pdf')
 plt.close()
 
-plt.plot(E1)
-plt.plot(E2)
-plt.plot(E3)
+plt.plot(t, E1, label = 'E1')
+plt.plot(t, E2, label = 'E2')
+plt.plot(t, E3, label = 'E3')
+plt.title('Energia Ek en funcion del tiempo')
+plt.legend()
+plt.xlabel('tiempo')
+plt.ylabel('Ek')
 plt.savefig('energias.pdf')
